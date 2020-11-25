44dde94d27c42d33ce9a86e8a6b72fe5e26bdbd6
Tahara <wants@studiom-web.net>
2020-11-25 16:25:46 +0900

    signup 完了

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 
 
 def todo(request):
     return HttpResponse('Hello!')
+
+
+def signupview(request):
+    print(request.method)
+    if request.method == 'POST':
+        username_data = request.POST['username_data']
+        password_data = request.POST['password_data']
+        try:
+            user = User.objects.create_user(username_data, '', password_data)
+        except IntegrityError:
+            return render(request, 'signup.html', {'error': 'このユーザーは既に登録されています'})
+    else:
+        print(User.objects.all())
+    return render(request, 'signup.html', {})
